1c38ac5900e33b93a3a0ee8968e5c525d324b421
w3bb01 <w3bb01@users.noreply.github.com>
2018-05-22 16:15:45 +0100

    Updated config with comments

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	steamKey: "",
-	priceKey: ""
+	steamKey: "", //your steam api key can be found at https://steamcommunity.com/dev/apikey
+	priceKey: "" //your pricing api key can be bought or rented from nicklason (the server owner) at https://discord.gg/TUm3zAc
 }
