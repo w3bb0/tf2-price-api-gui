dcca1c00ccb5299c985006ea099c0146888500ab
w3bb01 <w3bb01@users.noreply.github.com>
2018-05-30 15:14:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -213,7 +213,7 @@ function addItem(item) {
         request(options, function(error, response, body) {
             if (error) throw new Error(error);
             if (body.response.success != true) {
-                reject("had issues adding item to bot" + body.response[1])
+                reject("had issues adding item to bot" + body.response)
                 return
             }
             resolve(body.response.success)
